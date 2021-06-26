08767c9f8cf62de9145d97c99aab0d3fce97972a
nianny <xiaonianhe@hotmail.com>
2021-06-26 10:25:31 +0800

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Xiao Nianhe on 26/6/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
