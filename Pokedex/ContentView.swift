a999fc3ec2034d39ae79e329f769dfe5a586eb2f
nianny <xiaonianhe@hotmail.com>
2021-06-26 12:36:38 +0800

    pain

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -6,9 +6,10 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
-    
+//    let db = Firestore.firestore()
     var friends = [Friend(name: "YJ", icon: "zzz", school: "Tinkercademy", slothImage: "sloth3", age: 45), Friend(name: "Jia Chen", icon: "swift", school: "Ngee Ann Poly", slothImage: "sloth2", age: 17), Friend(name: "Ruirui", icon: "wifi", school: "NUS High", slothImage: "sloth1", age: 14)]
     
     var body: some View {
